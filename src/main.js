cef0182a98d9d28c6db160a11ccb720ae138edfd
Roger Killer <roger.killer@advery.ch>
2021-10-13 21:08:00 +0200

    import json server and setup buefy

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+createApp(App)
+  .use(store)
+  .use(router)
+  .mount("#app");
 
-createApp(App).use(store).use(router).mount('#app')
+// import buefy
+import Vue from "vue";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
+Vue.use(Buefy);
