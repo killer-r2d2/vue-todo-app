9feb4512ed4fc15233f60999233bad8e98fb801c
Roger Killer <roger.killer@advery.ch>
2021-10-22 14:20:38 +0200

    create key event ond input field

--- src/components/InputContainer.vue
+++ src/components/InputContainer.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="container">
-    <div class="notification is-primary">
-      This container is <strong>centered</strong> on desktop and larger
-      viewports.
-    </div>
+    <img  :src="image" alt="">
+    <form>
+      <label for="fname">Todo</label>
+      <input @keypress.enter="addToList" type="text" id="todo" name="todo" />
+    </form>
+    <div>Message is: {{ message }}</div>
+    <ul>
+      <li
+       v-for="todo in todos"
+       :key="todo.id"
+       class="listItem">{{ todo }}
+       </li>
+
+    </ul>
+    <div class="cart">Todo's({{ cart }})</div>
+    <button class="button" v-on:click="addToList">Add to list</button>
   </div>
 </template>
 
@@ -13,7 +25,24 @@ export default {
   props: {
     msg: String,
   },
+  data() {
+    return {
+      image: './assets/logo.png',
+      todoItem: "",
+      cart: 0,
+      todos: ["wash", "cook", "sing", "swimm"],
+      message: "",
+    }
+  },
+  methods: {
+    addToList: function () {
+      alert("addToList works");
+    }
+  }
 };
+
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only 
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "@/views/Home.vue";
 import Todo from "@/views/Todo.vue";
 import About from "@/views/Todo.vue";
 
